2e59ef344975d8e6a854c1f8a64e8a5df011237a
Alejandro Gutierrez <alegtz@Alejandros-MacBook-Pro-2.local>
2017-08-03 17:17:19 -0500

    yaml file fixed

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :brand
-  validates :price, numericality: true, presence: true
-  validates :price, numericality: {less_than_or_equal_to: 100}
+  validates :price, presence: true, numericality: {less_than_or_equal_to: 100}
   validates :name, presence: true
 end
